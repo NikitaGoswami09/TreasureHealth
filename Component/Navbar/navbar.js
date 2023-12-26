5e228ceadc323d05c83c9612164bb54067f8315e
Amit Kumar <amitkumar655921@gmail.com>
2023-12-26 16:33:34 +0530

    added navbar component

---
+++ Component/Navbar/navbar.js
@@ -0,0 +1,12 @@
+function Navbar() {
+    let NavbarContainer = document.createElement('div');
+    NavbarContainer.style.border = '1px solid black'
+    console.log(NavbarContainer)
+    return NavbarContainer
+    // html like 
+}
+
+// Export Navbar from this file
+// syntax
+
+export {Navbar}
