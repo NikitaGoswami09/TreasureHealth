d095eacd14d53625d0cc091fdbdf5015b52ffc49
NikitaGoswami09 <nikitagoswami09@gmail.com>
2024-01-03 17:38:06 +0530

    Deleted class.js and added navbar.js

--- Component/Navbar.js
+++
@@ -1,7 +0,0 @@
-function Navbar()
-{
-    let navbarContainer=document.createElement('div')
-    navbarContainer.style.border = "1px solid black"
-    return navbarContainer
-}
-export{Navbar}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Footer } from "./Component/Footer/Footer.js";
 let root = document.getElementById("root");
 import { Banner } from "./Component/Banner.js";
 import { Slider } from "./Component/slider.js";
+import{Navbar} from "./Component/Navbar/navbar.js"
 function App() {
   let sliderData = {
     cardArr: [
@@ -10,12 +11,14 @@ function App() {
       { cardTitle: "card title2" },
       { cardTitle: "card title3" },
       { cardTitle: "card title3" },
-    ],
+    ]
   };
+  root.appendChild(Navbar());
   root.appendChild(Banner());
   root.appendChild(Slider(sliderData, false));
   root.appendChild(Slider(sliderData, false));
   root.appendChild(Slider(sliderData, true));
   root.appendChild(Footer());
 }
-App();
+App()
+;
