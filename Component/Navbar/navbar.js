e388567aeff3becfa1955a65bdda559bbee76bb6
NikitaGoswami09 <nikitagoswami09@gmail.com>
2024-01-04 17:27:25 +0530

    navbar updated

--- Component/Navbar/navbar.js
+++ Component/Navbar/navbar.js
@@ -1,12 +1,21 @@
 function Navbar() {
 
   let NavbarContainer = document.createElement("nav");
-  NavbarContainer.innerHTML = `<a href="#logo">Logo</a>
+  NavbarContainer.innerHTML = `
+  
+  <div>
+  <a href="#logo">Logo</a>
+  </div>
+  <div>
   <a href="#Home">Home</a>
   <a href="#Products&services">Products & Services</a>
   <a href="#Events">Events</a>
   <a href="#ContactUs">ContactUs</a>
   <a href="#Blogs">Blogs</a>
+  </div>
+  <div>
+  User
+  </div>
 `
   NavbarContainer.style.border = "1px solid black";
   return NavbarContainer;
