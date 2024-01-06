d812209f2e7fd16f1ec2c16b0a4e83fe8220c441
Amit Kumar <amitkumar655921@gmail.com>
2024-01-06 16:36:05 +0530

    improved Slider.js

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-
 import { Footer } from "./Component/Footer/Footer.js";
 let root = document.getElementById("root");
 import { Banner } from "./Component/Banner.js";
 import { Slider } from "./Component/slider.js";
-import{Navbar} from "./Component/Navbar/navbar.js"
+import { Navbar } from "./Component/Navbar/navbar.js";
+
 function App() {
-  let sliderData = {
-    cardArr: [
-      { cardTitle: "card title1" },
-      { cardTitle: "card title2" },
-      { cardTitle: "card title3" },
-      { cardTitle: "card title3" },
-    ]
-  };
   root.appendChild(Navbar());
   root.appendChild(Banner());
-  root.appendChild(Slider(sliderData, false));
-  root.appendChild(Slider(sliderData, false));
-  root.appendChild(Slider(sliderData, true));
+  root.appendChild(Slider());
+  root.appendChild(Slider());
+  root.appendChild(Slider());
   root.appendChild(Footer());
 }
-App()
-;
+App();
