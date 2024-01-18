2e8a70bcd41d8fe9981dad2010cf846d9db88e23
Amit Kumar <amitkumar655921@gmail.com>
2024-01-18 22:37:35 +0530

    banner made and slider css added

--- Component/Banner.js
+++ Component/Banner.js
@@ -1,10 +1,26 @@
-
 function Banner() {
+  let bannerData;
+  async function getData() {
+    let res = await fetch("http://localhost:3000/banner");
+    let res2 = await res.json();
+    bannerData = res2[0];
+    console.log(bannerData.imgSrc);
+    renderBanner(bannerData.imgSrc, bannerData.heading);
+  }
+  getData();
   let BannerContainer = document.createElement("div");
-  BannerContainer.classList.add('bannerContainer')
-  BannerContainer.innerHTML = `
-    <h1>Banner</h1>
-    `;
+  BannerContainer.classList.add("bannerContainer");
+
+  function renderBanner(imgSrc, heading) {
+    BannerContainer.innerHTML = `
+      <div class="bannerImgContainer">
+        <img src="${imgSrc}" />
+      </div>
+      <div class="banner1Text">
+        <h2 class="banner1Heading">${heading}</h2>
+      </div>
+      `;
+  }
   return BannerContainer;
 }
-export{Banner}
+export { Banner };
